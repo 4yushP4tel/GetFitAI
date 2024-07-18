9006660bcc31d55520d0a7c9c27d782ccb258d5e
Ayush <payush548@gmail.com>
2024-07-18 13:47:55 -0400

    first commit

---
+++ fitness.py
@@ -0,0 +1,257 @@
+#imports OpenAI class from openai library
+# the api response is sent in a JSON and contains lots of info, including a choices list, which contains multiple different responses
+from openai import OpenAI
+from flask import Flask, render_template, request, url_for, redirect
+import os
+from dotenv import load_dotenv, find_dotenv
+
+#defines path to find the dotenv in directory
+dotenv_path = find_dotenv()
+
+#loads the variables from the .env file
+load_dotenv(dotenv_path)
+
+#store .env file's variable in a python variable
+get_fit_key = os.getenv("api_key")
+
+#api key is used to authenticate who uses the api,
+#this must stay hidden so instead of hardcoding it in my source code, I make it and environment variable using .env file
+
+
+# instance of OpenAI class is assigned to client variable
+client = OpenAI(api_key= get_fit_key)
+
+app = Flask(__name__, static_url_path='/static')
+
+def get_response(prompt):
+        completion = client.chat.completions.create(
+            model= "gpt-4o",
+            messages= [
+                {"role": "system", "content": "You are a fitness expert that gives workout advice to people in code"},
+                {"role": "user", "content": prompt}
+            ],
+            temperature = 0.05
+        )
+        return completion.choices[0].message.content
+
+@app.route("/home")
+def main():
+    return render_template("main.html")
+
+#repeat in the function below that all inputs must be filled, if not redirect back to the page
+@app.route("/workout_plan_generator", methods= ["GET","POST"])
+def plan():
+    if request.method == "POST":
+        try:
+            #weight
+            wt_unit = request.form["weight_unit"]
+            if wt_unit == "kg":
+                wt_in_kg_ = request.form["weight_kg"]
+                if not wt_in_kg_ :
+                    #uses query parameters to keep the already-inputted values in the form
+                    return redirect(url_for("plan"))
+            elif wt_unit == "lbs":
+                wt_in_lb_ = request.form["weight_lb"]
+                if not wt_in_lb_:
+                    return redirect(url_for("plan" ))
+                wt_in_lb_ = int(wt_in_lb_)
+                wt_in_kg_ = float(wt_in_lb_) * 0.453592
+                wt_in_kg_ = str(wt_in_kg_)
+            #age
+            age = request.form["age_name"]
+
+            #height
+            ht_unit = request.form["height_unit"]
+            if ht_unit == "meters":
+                ht_in_m_ = request.form["height"]
+                if not ht_in_m_ :
+                    return redirect(url_for("plan"))
+            elif ht_unit == "feet":
+                inches = request.form["height_inches"]
+                feet = request.form["height_feet"]
+                if not inches or not feet:
+                    return redirect(url_for("plan"))
+                #can't change from string to float, but can change form int to float
+                inches = int(inches)
+                feet = int(feet)
+                ht_in_in = inches + feet*12
+                ht_in_m_ = float(ht_in_in)*0.0254
+                ht_in_m_ = str(ht_in_m_)
+
+            #activity level
+            act_level = request.form["act_level"]
+
+            #activity type
+            activity_type = request.form["activity_type"]
+
+            #fitness goal
+            fitness_goal = request.form["fit_goal"]
+
+            #days
+            days = request.form["num_of_days"]
+            num_rest = 7- int(days)
+            num_rest = str(num_rest)
+
+            if not (age and act_level and activity_type and fitness_goal and days):
+                # brings the client back to the form but erases already filled things 
+                return redirect(url_for("plan")) 
+            
+            return redirect(url_for("your_plan", wt_in_kg=wt_in_kg_, ag=age, ht_in_m=ht_in_m_, act_lvl=act_level, act_type=activity_type, fit_goal=fitness_goal, num_days=days, rest_days = num_rest)) # add the info from the form
+        except:
+            #if error occurs, redirect to plan page
+            print("an error has occured")
+            return redirect(url_for("plan"))
+    else:
+            #if form not submitted just render the plan template
+        print("get method used")
+        return render_template("plan.html") 
+    
+@app.route("/macronutrient_calculator" , methods = ["GET", "POST"])
+def nutrition():
+    if request.method == "POST":
+        try:
+            #weight
+            weight_unit = request.form["weight_units"]
+            if weight_unit == "kilo":
+                weight_in_kg = request.form["weight_kgs"]
+                if not weight_in_kg:
+                    print("weight kg not submitted")
+                    return redirect(url_for("nutrition"))
+            elif weight_unit == "pound":
+                weight_in_lbs = request.form["weight_lbs"]
+                if not weight_in_lbs:
+                    print("weight lbs not submitted")
+                    return redirect(url_for("nutrition"))
+                weight_in_lbs = int(weight_in_lbs)
+                weight_in_kg = float(weight_in_lbs) * 0.453592
+                weight_in_kg = str(weight_in_kg)
+            
+            #age
+            age_value = request.form["age_value"]
+            if not age_value:
+                print("age not submitted")
+                return redirect(url_for("nutrition"))
+            #height
+            height_units = request.form["height_units"]
+            if height_units == "meters":
+                height_in_meters = request.form["height_in_meters"]
+                if not height_in_meters:
+                    print("height m not submitted")
+                    return redirect(url_for("nutrition"))
+            elif height_units == "feets":
+                height_value_feets = request.form["height_value_feets"]
+                height_value_inches =  request.form["height_value_inches"]
+                if not(height_value_feets or height_value_inches):
+                    print("height feet not submitted")
+                    return redirect(url_for("nutrition"))
+                height_value_feets = int(height_value_feets)
+                height_value_inches = int(height_value_inches)
+                height_value_inches = height_value_inches + height_value_feets*12
+                height_in_meters = float(height_value_inches)*0.0254
+                height_in_meters = str(height_in_meters)
+            #activity_level
+            activity_level = request.form["activity_level_value"]
+            if not activity_level:
+                print("activity level not submitted")
+                return redirect(url_for("nutrition"))
+            # weight_change
+            weight_change_unit = request.form["weight_change_unit"]
+            if weight_change_unit == "kg_change_value":
+                weight_change_kg = request.form["weight_change_kg"]
+                if not weight_change_kg:
+                    print("weight change kg not submitted")
+                    return redirect(url_for("nutrition"))
+            elif weight_change_unit == "lbs_change_value":
+                weight_change_lbs = request.form["weight_change_lbs"]
+                if not weight_change_lbs:
+                    print("weight change lbs not submitted")
+                    return redirect(url_for("nutrition"))
+                # page kept refreshing here due to not taking into account that the weight change in a float and not an integer
+                weight_change_lbs = float(weight_change_lbs)
+                weight_change_kg = weight_change_lbs* 0.453592
+                weight_change_kg = str(weight_change_kg)
+
+
+            return redirect(url_for("your_macros", wt_kg=weight_in_kg, age_number=age_value, ht_meters=height_in_meters, activity_lvl=activity_level, wt_change=weight_change_kg), code=302)
+                   
+        except:
+            print("an error occured")
+            return redirect(url_for("nutrition"))
+    else:  
+        print("could not give new data")    
+        return render_template("nutrition.html")
+
+@app.route("/your_plan")
+def your_plan():
+    
+    #takes value from query (search_bar)
+    wt_in_kg = request.args.get('wt_in_kg')
+    ag = request.args.get('ag')
+    ht_in_m = request.args.get('ht_in_m')
+    act_lvl = request.args.get('act_lvl')
+    act_type = request.args.get('act_type')
+    fit_goal = request.args.get('fit_goal')
+    num_days = request.args.get('num_days')
+    rest_days = request.args.get('rest_days')
+    
+    plan_prompt = f"""In just a table format (no other text) (in HTML code), please give me a full week of a workout plan including rest days(according to info given below) and workouts containing 3-5 exercises for the following person:
+                age: {ag},
+                weight: {wt_in_kg}kg,
+                height: {ht_in_m}m,
+                activity_level: {act_lvl},
+                desired activity type: {act_type},
+                number workouts per week desired: {num_days},
+                number of rest days per week desired: {rest_days}
+                fitness goal: {fit_goal}
+                
+                Rest days are only if the number of workouts per week is not 7. If it is not 7, then only add rest days according to the number of days
+                that the user wishes not to workout on. For example, if the number of workouts per week desired is 6 only add 1 rest day.
+                The table should have one row for each day of the week and columns titled respectively: day, exercises, set x reps, note
+                If a workout contains multiple exercises, it should still be included in the same row of the day, but
+                Each exercise should be in its own row
+                Every exercise should include a note
+                Do not include the title html and ''' in the response
+                At the bottom of the table include a small paragraph with a short sentence that should motivate someone to continue this workout plan
+                this short paragraph should have a left margin of 160px and a font size of 25px
+                """ # here I would add whatever html code that I want(in the future it will be whatever info I get from the API)
+    #print(new_html)
+    
+    plan_response = get_response(plan_prompt)
+    print(plan_response)
+    return render_template("your_plan.html", plan_response = plan_response)
+
+
+@app.route("/your_macros")
+def your_macros():
+    # takes values from query
+    wt_kg = request.args.get('wt_kg')
+    age_number = request.args.get('age_number')
+    ht_meters = request.args.get('ht_meters')
+    activity_lvl = request.args.get('activity_lvl')
+    wt_change = request.args.get('wt_change')
+    macro_prompt = f"""In just a table format (no other text), please give me the calories and macronutrients in HTML code for the following person:
+                age: {age_number},
+                weight: {wt_kg}kg,
+                height: {ht_meters}m,
+                activity_level: {activity_lvl},
+                weight change desired: {wt_change}kg (take into account the positive or negative sign)
+                Do not include the title html or ''' in the reponse
+                The table should have rows labeled calories, fats, protein, carbohydrates and columns named macronutrient and amount
+                At the bottom of the table include a small paragraph with a short sentence that should motivate someone to continue this diet
+                this short paragraph should have a left margin of 160px and a font size of 25px
+                """
+    macro_response = get_response(macro_prompt)
+    print(macro_response)
+    return render_template("your_macros.html" , macro_response = macro_response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#my_api_key = ""
+#openai.api_key = os.getenv(my_api_key)
+
+#print("Open ai is downloaded")
+#def get_results(prompt):
+#    pass
+
+
---
+++ static/new.js
@@ -0,0 +1,389 @@
+// For plan.html
+// display feet/inches when feet is checked, meters when meters is checked
+function display_height(){
+    let feet_display = document.getElementById("feet_input");
+    let meters_display = document.getElementById("meters_input");
+    let display_choice = document.getElementsByName("height_unit");
+    let selected_display;
+
+    for(let choice of display_choice){
+        if(choice.checked){
+            selected_display = choice.value;
+        }
+
+    }
+    
+    if(selected_display === "meters"){
+        feet_display.style.display = "none";
+        meters_display.style.display = "flex";
+    }
+    if(selected_display === "feet"){
+        feet_display.style.display = "flex";
+        meters_display.style.display = "none";
+    }
+}
+/* below, we call the function as soon as the page loads and
+when the radio box is changed, we run the function
+*/
+window.addEventListener("load", display_height);
+
+let radios = document.getElementsByName("height_unit");
+for(let radio of radios){
+    radio.addEventListener("change", display_height);
+}
+// function to choose between kg and lbs
+function weight_display(){
+    let kg_display = document.getElementById("kg_number");
+    let lbs_display = document.getElementById("lb_number");
+    let choices = document.getElementsByName("weight_unit");
+    let selection;
+
+    for(let choice of choices){
+        if(choice.checked){
+        selection = choice.value;
+        }
+    }
+        
+    if(selection === "lbs"){
+        kg_display.style.display = "none";
+        lbs_display.style.display = "block";
+    }
+            
+    if(selection === "kg"){
+        kg_display.style.display = "block";
+        lbs_display.style.display = "none";
+    }
+    
+}
+
+window.addEventListener("load", weight_display);
+
+let weight_choice = document.getElementsByName("weight_unit");
+for(let choice of weight_choice){
+    choice.addEventListener("change", weight_display);
+}
+
+function store_plan_inputs(){
+    let activity_level = document.getElementById("activity_level_choice");
+    let activity_type = document.getElementById("activity_type_choice");
+    let goal = document.getElementById("goal_choice");
+    let days = document.getElementById("num_days");
+    let age = document.getElementById("age");
+
+
+    let activity_level_value = activity_level.value;
+    let activity_type_value = activity_type.value;
+    let goal_value = goal.value;
+    let days_value = days.value;
+    age = age.value;
+
+    if(activity_level_value ==="" || activity_type_value ==="" || goal_value ==="" || days_value ==="" || age === "" ){
+        alert("All fields must be filled");
+        return;
+    }
+    if(age>100 || age<15 || !Number.isInteger(Number(age))){
+        alert("Please enter a valid age");
+        return;
+    }
+    if(days_value > 7 || !Number.isInteger(Number(days_value)) || days_value < 1){
+        alert("Please enter a valid number of days that you wish to workout");
+        return;
+    }
+
+    // height
+    let height_choice = document.getElementsByName("height_unit");
+    let height_unit;
+
+    for(let choice of height_choice){
+        if(choice.checked){
+            height_unit = choice.value;
+        }
+
+    }
+    let height;
+    if(height_unit === "feet"){
+        let feet = document.getElementById("feets").value;
+        let inches = document.getElementById("inches").value;
+        height = `${feet} feet and ${inches} inches`;
+        if(feet ==="" || inches ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(feet<1 || !Number.isInteger(Number(feet)) || feet > 8 || inches < 0 || inches > 11 || !Number.isInteger(Number(inches))){
+            alert("Please enter a valid height");
+            return;
+        }
+
+
+    }
+    if(height_unit === "meters"){
+        let meter = document.getElementById("meter_number").value;
+        height = `${meter} meters`;
+        if(meter ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(meter>2.50 || meter<1.00){
+            alert("Please enter a valid height");
+            return;
+        }
+
+    }
+
+    // weight
+    let weight_unit = document.getElementsByName("weight_unit");
+    let selected_weight_unit;
+
+    for(let unit of weight_unit){
+        if(unit.checked){
+        selected_weight_unit= unit.value;
+        }
+    }
+      
+    let weight;
+    if(selected_weight_unit === "lbs"){
+        lb = document.getElementById("lb_number").value;
+        weight = `${lb} lbs`;
+        if(lb ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(lb>300 || lb<90){
+            alert("Please enter a valid weight")
+            return;
+        }
+    }
+            
+    if(selected_weight_unit === "kg"){
+        kg = document.getElementById("kg_number").value;
+        weight = `${kg} kg`;
+        if(kg ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(kg>150 || kg<45){
+            alert("Please enter a valid weight")
+            return;
+        }
+    }
+
+    let plan_output = (`Give me in just a table format (No text before or after, just table) a ${days_value} day workout plan with exercises, sets and reps including all the days of the week for someone who is ${age} years old, measures ${height}, weighs ${weight}, who is ${activity_level_value}, wants to do stricly ${activity_type_value} and has for goal to ${goal_value} in html form`);
+    console.log(plan_output);
+}
+// for nutrition.html
+function weight_change_nutrition_display(){
+   
+    //weight change display
+    let kg_change_display = document.getElementById("kg_change_number");
+    let lbs_change_display = document.getElementById("lb_change_number");
+    let change_choices = document.getElementsByName("weight_change_unit");
+    let change_selection;
+
+    for(let change_choice of change_choices){
+        if(change_choice.checked){
+            change_selection = change_choice.value;
+        }
+        if(change_selection === "lbs_change_value"){
+            kg_change_display.style.display = "none";
+            lbs_change_display.style.display = "block";
+        }
+        if(change_selection === "kg_change_value"){
+            kg_change_display.style.display = "block";
+            lbs_change_display.style.display = "none";
+        }
+    }
+}
+window.addEventListener("load", weight_change_nutrition_display);
+
+let change_choices = document.getElementsByName("weight_change_unit");
+for(let change_choice of change_choices){
+    change_choice.addEventListener("change", weight_change_nutrition_display);
+}
+
+
+
+function height_nutrition_display(){
+ //height_display
+    let feet_display = document.getElementById("feets_input");
+    let meters_display = document.getElementById("meters_input");
+    let display_choice = document.getElementsByName("height_units");
+    let selected_display;
+
+    for(let choice of display_choice){
+        if(choice.checked){
+            selected_display = choice.value;
+        }
+
+    }
+    
+    if(selected_display === "meters"){
+        feet_display.style.display = "none";
+        meters_display.style.display = "flex";
+    }
+    if(selected_display === "feets"){
+        feet_display.style.display = "flex";
+        meters_display.style.display = "none";
+    }
+
+}
+window.addEventListener("load", height_nutrition_display);
+
+let height_nutrition_choices = document.getElementsByName("height_units");
+for(let height_nutrition_choice of height_nutrition_choices){
+    height_nutrition_choice.addEventListener("change", height_nutrition_display);
+}
+
+
+function weight_nutrition_display(){
+    let kg_display = document.getElementById("kg_number");
+    let lbs_display = document.getElementById("lb_number");
+    let choices = document.getElementsByName("weight_units");
+    let selection;
+
+    for(let choice of choices){
+        if(choice.checked){
+        selection = choice.value;
+        }
+    }
+        
+    if(selection === "pound"){
+        kg_display.style.display = "none";
+        lbs_display.style.display = "block";
+    }
+            
+    if(selection === "kilo"){
+        kg_display.style.display = "block";
+        lbs_display.style.display = "none";
+    }
+    
+}
+
+window.addEventListener("load", weight_nutrition_display);
+
+let weight_nutrition_choices = document.getElementsByName("weight_units");
+for(let weight_nutrition_choice of weight_nutrition_choices){
+    weight_nutrition_choice.addEventListener("change", weight_nutrition_display);
+}
+
+function store_nutrition_inputs(){
+    let activity_level_value = document.getElementById("activity_level_choice").value;
+    let age = document.getElementById("age").value;
+    //height
+    let height_choice = document.getElementsByName("height_units");
+    let height_unit;
+
+    if(activity_level_value ==="" || age ===""){
+        alert("All fields must be filled");
+        return;
+    }
+
+    if(age>100 || age<15 || !Number.isInteger(Number(age))){
+        alert("Please enter a valid age");
+        return;
+    }
+
+    for(let choice of height_choice){
+        if(choice.checked){
+            height_unit = choice.value;
+        }
+
+    }
+    let height;
+    if(height_unit === "feets"){
+        let feet = document.getElementById("feets").value;
+        let inches = document.getElementById("inches").value;
+        height = `${feet} feet and ${inches} inches`;
+        if(feet ==="" || inches ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(feet<1 || !Number.isInteger(Number(feet)) || feet > 8 || inches < 0 || inches > 11 || !Number.isInteger(Number(inches))){
+            alert("Please enter a valid height");
+            return;
+        }
+    }
+    if(height_unit === "meters"){
+        meter = document.getElementById("meter_number").value;
+        height = `${meter} meters`;
+        if(meter ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(meter>2.50 || meter<1.00){
+            alert("Please enter a valid height");
+            return;
+        }
+    }
+    //weight
+    let weight_unit = document.getElementsByName("weight_units");
+    let selected_weight_unit;
+
+    for(let unit of weight_unit){
+        if(unit.checked){
+        selected_weight_unit= unit.value;
+        }
+    }
+      
+    let weight;
+    if(selected_weight_unit === "pound"){
+        lb = document.getElementById("lb_number").value;
+        weight = `${lb} lbs`;
+        if(lb ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(lb>300 || lb < 90){
+            alert("Please input a valid weight");
+            return;
+        }
+    }
+            
+    if(selected_weight_unit === "kilo"){
+        kg = document.getElementById("kg_number").value;
+        weight = `${kg} kg`;
+        if(kg ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(kg>150 || kg< 45){
+            alert("Please input a valid weight");
+            return;
+        }
+    }
+    //weight change
+    let weight_change_units = document.getElementsByName("weight_change_unit");
+    let change_unit;
+    let weight_change;
+    for(let units of weight_change_units){
+        if(units.checked){
+            change_unit = units.value;
+        }
+    }
+    if(change_unit === "lbs_change_value"){
+        lb_change = document.getElementById("lb_change_number").value;
+        weight_change = `${lb_change} lbs`;
+        if(lb_change ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(lb_change>5 || lb_change < -5){
+            alert("You can not healthily lose that much weight every week, Please enter a valid number");
+            return;
+        }
+    }
+    if(change_unit === "kg_change_value"){
+        kg_change = document.getElementById("kg_change_number").value;
+        weight_change = `${kg_change} kg`;
+        if(kg_change ===""){
+            alert("All fields must be filled");
+            return;
+        }
+        if(kg_change>3 || kg_change< -3){
+            alert("You can not healthily lose that much weight every week, Please enter a valid number");
+            return;
+        }
+    }
+
+    let nutrition_output = (`Give me just the numbers in table format (no calculations at all) of the macronutrients and calories that someone who is ${age}years old, ${height} tall, weighs ${weight} and wishes to change their weight by ${weight_change} per week and has a ${activity_level_value} activity level should consume. In html form`);
+    console.log(nutrition_output);   
+}
